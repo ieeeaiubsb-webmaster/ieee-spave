bfb567dd9e87b5d14b972d238224e511dd428b36
shifat-arman <shifatshiam@gmail.com>
2025-10-02 14:04:41 +0600

    fix: affiliation centering on desktop

--- src/components/Affiliation.tsx
+++ src/components/Affiliation.tsx
@@ -66,7 +66,7 @@ export default function Affiliation() {
   ];
 
   return (
-    <div className={`container`}>
+    <div className={`flex justify-center w-full py-8`}>
       <Marquee>
         {logos.map((logo, index) => (
           <Image
